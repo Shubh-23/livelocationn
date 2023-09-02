9a8107be483070cea82c31f3010a760b4076b245
shubham <potterparker09@gmail.com>
2023-09-02 14:06:08 +0530

    Chagne done

---
+++ index.js
@@ -0,0 +1,39 @@
+const express = require('express');
+const app = express();
+const http = require('http');
+const cors = require('cors');
+const server = http.createServer(app);
+const { Server } = require("socket.io");
+const io = new Server(server);
+
+
+app.use(cors({
+    origin: '*'
+}));
+
+app.get('/api', (req, res) => {
+    console.log("data");
+    return res.json("hello")
+//   res.sendFile(__dirname + '/index.html');
+});
+
+io.on('connection', (socket) => {
+//   console.log('a user connected',socket.id);
+  var latLong
+  socket.on('createMessage', (newMessage)=>{
+    // console.log('newMessage', newMessage);
+    latLong = newMessage
+console.log(newMessage);
+    io.emit('get_msg', newMessage);
+    // console.log(socket.broadcast.emit('get_msg',latLong),latLong);
+    // socket.broadcast.to(socket.id).emit('get_msg',latLong);
+  })
+//   socket.broadcast.emit("getLatLong", latLong);
+  
+});
+
+
+
+server.listen(3000, () => {
+  console.log('listening on *:3000');
+});
